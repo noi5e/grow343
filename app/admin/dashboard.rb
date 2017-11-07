e3f478ea21e67da4a54e9d6355524068ebf5fbd1
Gabriel Odess Gillett <gabeodess@gmail.com>
2017-11-06 21:29:59 -0600

    added charts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,35 +3,11 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
     if current_user.is_a?(Student)
       render 'student_dashboard'
     elsif current_user.is_a?(Teacher)
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    columns do
-      column do
-        panel "Recent Posts" do
-          ul do
-            # Post.recent(5).map do |post|
-            #   li link_to(post.title, admin_post_path(post))
-            # end
-          end
-        end
-      end
-
-      column do
-        panel "Info" do
-        end
-      end
-    end
   end # content
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,4 +1,5 @@
 //= require active_admin/base
+//= require google-chart
 
 // ========================
 // = Learning Target Form =
@@ -8,3 +9,35 @@ var AddResourceToLastObjective = function(){$('.has_many_container.learning_obje
 $(document).on('click', '.has_many_container.learning_objectives > .has_many_add', function(){
   setTimeout(AddResourceToLastObjective,100);
 });
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+
+function drawChart(d, id) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'X');
+  data.addColumn('number', 'Score');
+
+  data.addRows(d);
+
+  var options = {
+    pointSize: 4,
+    legend: {position: 'none'},
+    vAxis: {
+      baseline: 0,
+      gridlines: {color: 'transparent'},
+      viewWindow: {
+          min: 0,
+          max: 4
+      }
+    },
+    hAxis: {
+      baselineWidth: 2,
+      baselineColor: '#000',
+      gridlines: {color: '#000000'}
+    }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById(id));
+  chart.draw(data, options);
+}
+
