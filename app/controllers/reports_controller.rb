8c75c4a1e0512f090008156865f5b1704cb8efb7
scorinaldi <sharif.corinaldi@gmail.com>
2018-07-19 22:58:38 -0400

    created a printer friendly view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,39 +1,85 @@
 class ReportsController < ApplicationController
 
-  def learning_target_reports
+    GOOD_PERFORMANCE_COLOR = "#8daa92"
+    OK_PERFORMANCE_COLOR = "#e29b20"
+    BAD_PERFORMANCE_COLOR = "#d45f53"
 
-    # load the learning learning_target_id=16&q%5Bstudent_detail_teacher_id_eq%5D=10
+    def generate_csv_string(learning_target, student_tpls)
+        csv_string = CSV.generate do |csv|
+            # header row
+            csv << %w(Name V1 V2 V3)
 
-    # look up the appropriate grade for this learning target
-    learning_target = LearningTarget.find( params[:learning_target_id] )
+            # add a row for each item
+            student_tpls.each do |rr|
+                learning_target_tpls = LearningResult.find_by_sql "Select a.* from learning_results a where a.learning_target_id=#{params[:learning_target_id]} and a.student_id = #{rr.id}"
 
-    # look up student_id by teacher_id
-    student_tpls = Student.find_by_sql "Select b.* from student_details a, users b where b.id = a.student_id and a.teacher_id = #{params[:q][:student_detail_teacher_id_eq]} and type = 'Student' order by b.last_name, b.first_name"
+                # skip if the student's grade doesn't match this learning target's grade
+                next if learning_target.grade != Student.grade_by_graduation_year(rr.graduation_year)
 
-    csv_string = CSV.generate do |csv|
-        # header row
-        csv << %w(Name V1 V2 V3)
+                record_map = {}
+                learning_target_tpls.each do |lt|
+                    record_map[lt.version] = lt.score
+                end
+                # look up 3 Learning Results by student_id and learning_target_id
+                csv << ["#{rr.last_name}, #{rr.first_name}", record_map.fetch(1, "-"), record_map.fetch(2, "-"), record_map.fetch(3, "-")]
+            end
+        end
+        csv_string
+    end
 
-        # add a row for each item
+    # determine the display colors
+    def color_cues(record_map, current_key)
+        return OK_PERFORMANCE_COLOR if record_map.fetch(1, "-") == "-" ||  record_map.fetch(current_key, "-") == "-"
+
+        # compare current key to now
+        if record_map[current_key] > record_map[1]
+            return GOOD_PERFORMANCE_COLOR
+        elsif record_map[current_key] == record_map[1]
+            return OK_PERFORMANCE_COLOR
+        else
+            return BAD_PERFORMANCE_COLOR
+        end
+    end
+
+    # for rendering the print friendly version, we'll pre-gather all the values
+    def generate_learning_result_tuples(learning_target, student_tpls)
+        result_tpls = []
         student_tpls.each do |rr|
             learning_target_tpls = LearningResult.find_by_sql "Select a.* from learning_results a where a.learning_target_id=#{params[:learning_target_id]} and a.student_id = #{rr.id}"
 
-            # skip if the student's grade doesn't match this learning target's grade
+             # skip if the student's grade doesn't match this learning target's grade
             next if learning_target.grade != Student.grade_by_graduation_year(rr.graduation_year)
 
             record_map = {}
             learning_target_tpls.each do |lt|
                 record_map[lt.version] = lt.score
             end
-            # look up 3 Learning Results by student_id and learning_target_id
-            csv << ["#{rr.last_name}, #{rr.first_name}", record_map.fetch(1, "-"), record_map.fetch(2, "-"), record_map.fetch(3, "-")]
-        end
-    end
-    respond_to do |format|
-        format.csv do
-            send_data(csv_string, :filename => "learning_results_report.csv")
-        end
-    end
-  end
 
+            # look up 3 Learning Results by student_id and learning_target_id
+            color_2 =
+            result_tpls.push(["#{rr.last_name}, #{rr.first_name}", record_map.fetch(1, "-"), color_cues(record_map, 2), record_map.fetch(2, "-"), color_cues(record_map, 3), record_map.fetch(3, "-")])
+        end
+        result_tpls
+    end
+
+    # main erb method
+    def learning_target_reports
+
+        # look up the appropriate grade for this learning target
+        learning_target = LearningTarget.find( params[:learning_target_id] )
+
+        # look up student_id by teacher_id
+        student_tpls = Student.find_by_sql "Select b.* from student_details a, users b where b.id = a.student_id and a.teacher_id = #{params[:q][:student_detail_teacher_id_eq]} and type = 'Student' order by b.last_name, b.first_name"
+
+        respond_to do |format|
+            format.html do
+                results = generate_learning_result_tuples(learning_target, student_tpls)
+                render "learning_target_report", :layout => "printable.html.erb", :locals => { :result_tpls => results }
+            end
+            format.csv do
+                csv_string = generate_csv_string(learning_target, student_tpls)
+                send_data(csv_string, :filename => "learning_results_report.csv")
+            end
+        end
+    end
 end
